df5a81e1420c890fdb223c9a463a9291197c2b9d
nadolf <nadolftagoe1@gmail.com>
2022-07-14 14:31:58 -0400

    Background

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
-from numpy import append
 import pygame
 
 pygame.init()
 
 clock = pygame.time.Clock()
 FPS = 60
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 432
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
+scroll = 0
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Parallax")
+pygame.display.set_caption("The Adventures of Red Riding Hood!")
 
-bg_images = []
-for i in range (1,3):
-    bg_image=pygame.image.load(f"Assets/background_layer_{i}.png").convert_alpha()
-    bg_images.append(bg_image)
+background_imgs = []
+for i in range(1, 3):
+    background_image = pygame.image.load(
+        f"Assets/background_layer_{i}.png").convert_alpha()
+    background_imgs.append(background_image)
+    bg_width = background_imgs[0].get_width()
 
-def draw_bg():
-    for i in bg_images:
-        screen.blit(i, (0,0))
+
+def background():
+    for x in range(5):
+        speed = 1
+        for i in background_imgs:
+            screen.blit(i, ((x * bg_width) - scroll * speed, 0))
+            speed += 0.25
 
 run = True
 while run:
     clock.tick(FPS)
 
-    draw_bg()
+    background()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and scroll > 0:
+        scroll -= 5
+    if key[pygame.K_RIGHT] and scroll < 3000:
+        scroll += 5
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+
     pygame.display.update()
 pygame.quit()
